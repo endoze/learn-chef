5a58881e93f6e446c3782c1f53fb23852e3b334c
Chris Stephan <cstephan@squaremouth.com>
2014-04-07 21:09:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# learn-chef cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: learn-chef
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
