2076633149901147b5265df7144e2015dc929f10
Chris Stephan <cstephan@squaremouth.com>
2014-04-07 21:14:24 -0400

    Update metadata.rb with my information
    In order to claim this cookbooks as my own :smile:, this commit adds my
    information to the metadata.rb. It also adds a dependency of nginx and
    states that it will support centos, ubuntu, and debian.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,14 @@
 name             'learn-chef'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures learn-chef'
+maintainer       'CStephan'
+maintainer_email 'cstephan@squaremouth.com'
+license          'MIT'
+description      'Installs/Configures nginx to serve an application via reverse proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+
+depends 'nginx'
+
+%w{centos ubuntu debian}.each do |os|
+  supports os
+end
