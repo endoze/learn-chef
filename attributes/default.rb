64e43716e8037fa039b90f096fe581fbda56ce75
Chris Stephan <cstephan@squaremouth.com>
2014-04-08 20:55:04 -0400

    Add reverse proxy settings to default recipe
    In order to set up a reverse proxy, this commit adds attributes and a
    template resource to our default recipe. It also adds a template nginx
    config file.

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+
+default[:learn_chef][:app_directory]  = '/home/vagrant/'
+default[:learn_chef][:port]           = 4567
+default[:learn_chef][:default_server] = true
+default[:learn_chef][:server_name]    = 'learn-chef-berkshelf'
+default[:learn_chef][:force_ssl]      = false
+default[:learn_chef][:user]           = 'vagrant'
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,19 @@
 #
 
 include_recipe 'nginx'
+
+template '/etc/nginx/sites-enabled/learn_chef' do
+  source 'nginx.conf.erb'
+  owner 'root'
+  group 'root'
+  mode  0755
+  notifies :restart, 'service[nginx]'
+  variables(
+    :app_home       => node[:learn_chef][:app_directory],
+    :port           => node[:learn_chef][:port],
+    :app_name       => node[:learn_chef][:user],
+    :default_server => node[:learn_chef][:default_server],
+    :force_ssl      => false,
+    :server_name    => node[:learn_chef][:server_name],
+  )
+end
